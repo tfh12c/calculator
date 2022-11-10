e5041219ebde55284383887687ac004163409091
Travis Hunsberger <tfhunsberger@gmail.com>
2022-11-10 17:53:29 -0500

    js finished for switching between themes

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+const blueModeToggle = document.querySelector('#blue-mode-toggle');
+const lightModeToggle = document.querySelector('#light-mode-toggle');
+const purpleModeToggle = document.querySelector('#purple-mode-toggle');
+const htmlElement = document.querySelector('html');
+const equalKey = document.querySelector('.calculator__equals');
+
+window.onload = () => {
+    blueModeToggle.classList.add('active');
+}
+
+blueModeToggle.addEventListener('click', () =>{
+    htmlElement.removeAttribute('class');
+    blueModeToggle.classList.add('active');
+    lightModeToggle.classList.remove('active');
+    purpleModeToggle.classList.remove('active');
+})
+
+lightModeToggle.addEventListener('click', () =>{
+    htmlElement.removeAttribute('class');
+    htmlElement.classList.add('lightmode');
+    lightModeToggle.classList.add('active');
+    blueModeToggle.classList.remove('active');
+    purpleModeToggle.classList.remove('active');
+})
+
+purpleModeToggle.addEventListener('click', () => {
+    htmlElement.removeAttribute('class');
+    htmlElement.classList.add('purplemode');
+    purpleModeToggle.classList.add('active');
+    blueModeToggle.classList.remove('active');
+    lightModeToggle.classList.remove('active');
+    equalKey.style.color = 'hsl(198, 20%, 13%)';
+})
+
 const add = (num1, num2) => {
     const sum = (num1 + num2);
     return (Math.round((sum + Number.EPSILON) * 100) / 100);
